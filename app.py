c557b2a3cec3eca49f4d9e353844a360eda1f336
countingshep <52992076+countingshep@users.noreply.github.com>
2020-10-20 12:18:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,16 @@ outdoor2 = input("I am active.")
 
 music2 = input("I play a musical instrument well.")
 
+tech3 = input("I enjoy gaining new experiences.")
 
-tech_final = int(tech1) + int(tech2)
-outdoor_final = int(outdoor1) + int(outdoor2)
-music_final = int(music1)+ int(music2)
+outdoor3 = input("I enjoy playing sports games.")
+
+music3 = input("I enjoy listening to and playing music.")
+
+
+tech_final = int(tech1) + int(tech2) + int (tech3)
+outdoor_final = int(outdoor1) + int(outdoor2) + int(outdoor3)
+music_final = int(music1)+ int(music2) + int (music3)
 
 print()
 
@@ -30,4 +36,10 @@ elif outdoor_final > music_final:
 else:
   print("You might be suitable for Performing Arts!")
 
-  
+print( "This is because it was your highest scoring CCA type. Your competency for UG CCAs was " + str(tech_final) + ", your competency for sports CCAs was " + str(outdoor_final) + ", and your competency for Performing Arts was " + str(music_final) + ". Hope you enjoyed this:)" )
+
+
+
+
+
+
